634e734016494f4c7fabe8e0f6eb1b594a39770c
manuhx <manuelverron@gmail.com>
2018-01-03 09:41:59 +0100

    modification du template

---
+++ Habibi_AirLines/views/acceuil.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hx
+ * Date: 30/12/2017
+ * Time: 09:10
+ */
+
+//include "../templates/index_old.html";
+
+include "../templates/template_header.html";
+
+echo "Hello !";
+
+include "../templates/template_footer.html";
